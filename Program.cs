d69a57b9419e185149696f1c9e38cf1d1c31409f
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-07 15:24:33 -0300

    Moved player info

--- Program.cs
+++ Program.cs
@@ -10,27 +10,35 @@ namespace MiniGame_Loops_KatelynNicholson
     {
 
         static int playerPosition = 0;
+        static bool gameOver = false;
 
         static void Main()
         {
-
-            //player
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.SetCursorPosition(5,2);
-            Console.WriteLine("^ ^");
-            Console.SetCursorPosition(3,3);
-            Console.WriteLine("=[0.0]=");
-            Console.ForegroundColor = ConsoleColor.White;
-            //
-
-
-
             //move player using WASD
+            
+
             //have a trail follow player
             //if player exits pannel. you loose
             //hit escape to exit
             //keep trail visable and if player lands on a spot already marked then. you loose//visited
             //array playable area
+        }
+
+
+        static void PlayerDraw(int x, int y)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.SetCursorPosition(5, 2);
+            Console.WriteLine("^ ^");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("=[0.0]=");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void PlayerUpdate(int x,int y)
+        {
+            //console.readkey()
+            //console.keyinfor.keychar
         }
     }
 }
